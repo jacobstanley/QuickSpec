67cce8ce8561a3ae9118f5876b52a24fe9dd577e
Nick Smallbone <nicsma@chalmers.se>
2009-05-29 17:29:00 +0800

    Tweaking QuickSpec.
    Ignore-this: a916e74144ca8b8496c2da1c2cc4e8e3
    
    darcs-hash:20090529092900-4f525-ad31ee5140d012ef439b7172bd553cc4241e1c1e.gz

--- Equations.hs
+++ Equations.hs
@@ -456,7 +456,7 @@ laws ctx0 depth = do
                                     | otherwise = (x:xs) `isSublistOf` ys
     when (not (vars y `isSubsetOf` vars x || vars x `isSubsetOf` vars y)) $
          printf "*** missing term with value %s\n"
-                (show (mapVars (\s -> if s `elem` vars y then s else s { name = "_" }) x))
+                (show (mapVars (\s -> if s `elem` vars y then s else s { name = "_" ++ name s }) x))
 
 test :: Int -> Context -> Valuations -> Int -> (Type -> [Term Symbol]) -> IO (Int, [[Term Symbol]])
 test depth ctx vals start base = do
