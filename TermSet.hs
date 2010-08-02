c3a5636850ee2a91ef91eed8ba36111e45e2a2ff
Nick Smallbone <nicsma@chalmers.se>
2010-08-03 00:01:30 +0800

    Tentative stuff
    Ignore-this: 8c4cee2605aa82a07b2d7626ce70d7e4
    
    darcs-hash:20100802160130-4f525-523bd99060d5a7df383b77ca61b8b48771cc5ad2.gz

--- TermSet.hs
+++ TermSet.hs
@@ -1,5 +1,7 @@
-{-# LANGUAGE FlexibleContexts, MultiParamTypeClasses, TypeFamilies, TupleSections, ImplicitParams #-}
-import Control.Monad
+module TermSet where
+
+import TestTree
+
 -- WHAT WE SHOULD DO:
 -- separate testing universe from pruning universe.
 -- Describe pruning universe by describing the set of all
@@ -9,40 +11,6 @@ import Control.Monad
 -- the rules for conditionally-rigidified variables, etc.
 
 -- This structure supports instance generation.
-data Set tc a = Nil | Base (TestingTree tc a) | Apply (Set tc a) (Set tc a) -- | Var ...
+data Set tc a = Nil | Base (TestTree tc a) | Apply (Set tc a) (Set tc a) -- | Var ...
 -- Ah, so "Set a" can have the "more terms as you test more" structure
 -- but TestingTree shouldn't.
-
--- This structure supports class refinement, union etc.
--- Invariant: branches is sorted by comparing (eval testCase . rep).
-data TestingTree tc a = Tree { rep :: a, rest :: [a], testCase :: tc, branches :: [TestingTree tc a] }
-
-class Ord (Case tc a) => TestCase tc a where
-  type Case tc a
-  eval :: tc -> a -> Case tc a
-
-union :: TestCase tc a => TestingTree tc a -> TestingTree tc a -> TestingTree tc a
-union t1 t2 = Tree { rep = rep t1, rest = rest t1++rep t2:rest t2, testCase = testCase t1,
-                     branches = merge (branches t1) (branches t2) }
-  where merge [] ys = ys
-        merge xs [] = xs
-        merge (x:xs) (y:ys) | eval (testCase t1) (rep t1) < eval (testCase t2) (rep t2) = x:merge xs (y:ys)
-                            | eval (testCase t1) (rep t1) > eval (testCase t2) (rep t2) = y:merge (x:xs) ys
-                            | otherwise = union x y:merge xs ys
-
-partitionBy :: Ord b => (a -> b) -> [a] -> [[a]]
-partitionBy = undefined
-
--- Precondition: xs must be sorted.
-test :: TestCase tc a => [tc] -> [a] -> TestingTree tc a
-test _ [] = error "bug: an equivalence class can't be empty"
-test (tc:tcs) (x:xs) = Tree { rep = x, rest = xs, testCase = tc, branches = map (test tcs) bs }
-  where bs = partitionBy (eval tc) (x:xs)
-
-fringe :: TestingTree tc a -> Int
-fringe = maxNumber . cutOff 0
-  where cutOff 100 t = Tree { rep = rep t, rest = rest t, testCase = testCase t, branches = [] }
-        cutOff n t@Tree{branches = [t']} = t { branches = [cutOff (n+1) t'] }
-        cutOff n t = t { branches = map (cutOff 0) (branches t) }
-        maxNumber Tree{branches = []} = 0
-        maxNumber Tree{branches = bs} = 1+maximum (map maxNumber bs)
---
+++ TestTree.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE FlexibleContexts, MultiParamTypeClasses, TypeFamilies, TupleSections, ImplicitParams #-}
+module TestTree where
+
+-- This structure supports class refinement, union etc.
+-- Invariant: branches is sorted by comparing (eval testCase . rep).
+data TestTree tc a = Tree { rep :: a, rest :: [a], testCase :: tc, branches :: [TestTree tc a] }
+
+class Ord (Case tc a) => TestCase tc a where
+  type Case tc a
+  eval :: tc -> a -> Case tc a
+
+union :: TestCase tc a => TestTree tc a -> TestTree tc a -> TestTree tc a
+union t1 t2 = Tree { rep = rep t1, rest = rest t1++rep t2:rest t2, testCase = testCase t1,
+                     branches = merge (branches t1) (branches t2) }
+  where merge [] ys = ys
+        merge xs [] = xs
+        merge (x:xs) (y:ys) | eval (testCase t1) (rep t1) < eval (testCase t2) (rep t2) = x:merge xs (y:ys)
+                            | eval (testCase t1) (rep t1) > eval (testCase t2) (rep t2) = y:merge (x:xs) ys
+                            | otherwise = union x y:merge xs ys
+
+partitionBy :: Ord b => (a -> b) -> [a] -> [[a]]
+partitionBy = undefined
+
+-- Precondition: xs must be sorted.
+test :: TestCase tc a => [tc] -> [a] -> TestTree tc a
+test _ [] = error "bug: an equivalence class can't be empty"
+test (tc:tcs) (x:xs) = Tree { rep = x, rest = xs, testCase = tc, branches = map (test tcs) bs }
+  where bs = partitionBy (eval tc) (x:xs)
+
+fringe :: TestTree tc a -> Int
+fringe = maxNumber . cutOff 0
+  where cutOff 100 t = Tree { rep = rep t, rest = rest t, testCase = testCase t, branches = [] }
+        cutOff n t@Tree{branches = [t']} = t { branches = [cutOff (n+1) t'] }
+        cutOff n t = t { branches = map (cutOff 0) (branches t) }
+        maxNumber Tree{branches = []} = 0
+        maxNumber Tree{branches = bs} = 1+maximum (map maxNumber bs)
