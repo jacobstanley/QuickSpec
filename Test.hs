11781841a822984fdcede814c003deb3862a6b28
Nick Smallbone <nicsma@chalmers.se>
2010-08-05 01:07:11 +0800

    Switched to IntExprs instead of BoolExprs (more interesting)
    Ignore-this: 19d8c4a689bac96b488eac3cfc42c3b9
    
    darcs-hash:20100804170711-4f525-377fcc3fb0847dc8d12229719f6f84dfbd78673c.gz

--- Test.hs
+++ Test.hs
@@ -6,35 +6,34 @@ import System.Random
 import Test.QuickCheck hiding (numTests, Result)
 import Test.QuickCheck.Gen
 
-data BoolExpr = Var Var | T | F | And BoolExpr BoolExpr | Or BoolExpr BoolExpr | Not BoolExpr deriving (Eq, Ord, Show)
+data IntExpr = Var Var | Const Int | Plus IntExpr IntExpr | Times IntExpr IntExpr | Neg IntExpr deriving (Eq, Ord, Show)
 data Var = X | Y | Z deriving (Eq, Ord, Show)
 
 repeatM = sequence . repeat
 
-tests :: StdGen -> [(Bool, Bool, Bool)]
+tests :: StdGen -> [(Int, Int, Int)]
 tests g = unGen (repeatM arbitrary) g 5
 
-allTerms :: Int -> StdGen -> [BoolExpr]
-allTerms 0 _ = [Var X, Var Y, Var Z, T, F]
-allTerms (n+1) g = allTerms 0 g ++ liftM3 id [And, Or] atn atn ++ fmap Not atn
+allTerms :: Int -> StdGen -> [IntExpr]
+allTerms 0 _ = [Var X, Var Y, Var Z, Const 0, Const 1]
+allTerms (n+1) g = allTerms 0 g ++ liftM3 id [Plus, Times] atn atn ++ fmap Neg atn
   where atn = map head (classes (results n g))
 
-results :: Int -> StdGen -> TestResults BoolExpr
+results :: Int -> StdGen -> TestResults IntExpr
 results n g = cutOff 100 (test (tests g) (allTerms n g))
 
-instance Eval BoolExpr where
-  type TestCase BoolExpr = (Bool, Bool, Bool)
-  type Result BoolExpr = Bool
+instance Eval IntExpr where
+  type TestCase IntExpr = (Int, Int, Int)
+  type Result IntExpr = Int
   eval (x, _, _) (Var X) = x
   eval (_, y, _) (Var Y) = y
   eval (_, _, z) (Var Z) = z
-  eval _ T = True
-  eval _ F = False
-  eval e (And x y) = eval e x && eval e y
-  eval e (Or x y) = eval e x || eval e y
-  eval e (Not x) = not (eval e x)
+  eval _ (Const x) = x
+  eval e (Plus x y) = eval e x + eval e y
+  eval e (Times x y) = eval e x * eval e y
+  eval e (Neg x) = negate (eval e x)
 
 main = do
   seed <- newStdGen
-  let rs = results 3 seed
+  let rs = results 2 seed
   printf "%d terms, %d classes, %d tests\n" (length (concat (classes rs))) (length (classes rs)) (numTests rs)
