b5755c8352fb5935450cdd0149e71e7309889a5a
Nick Smallbone <nicsma@chalmers.se>
2010-08-23 21:58:08 +0800

    Using Term.hs in Test.hs
    Ignore-this: 6c71f490d1c99859f8e1679c7a636c9e
    
    darcs-hash:20100823135808-4f525-ba7eaf5c83e0e573972eecb833660a2fa44e4863.gz

--- Term.hs
+++ Term.hs
@@ -2,12 +2,26 @@
 module Term where
 
 import TestTree
+import Data.Ord
 
 data Args v a b where
   Nil :: Args v a a
   Snoc :: Term v a -> Args v b (a -> c) -> Args v b c
 
+instance Show (Args v a b) where
+  show Nil = ""
+  show (Snoc x as) = show as ++ "," ++ show x
+
 data Term v a = forall b. App { name :: String, fun :: (v -> b), args :: Args v b a }
+
+instance Eq (Term v a) where
+  t1 == t2 = show t1 == show t2
+instance Ord (Term v a) where
+  compare = comparing show
+
+instance Show (Term v a) where
+  show App { name = name, args = Nil } = name
+  show App { name = name, args = args } = name ++ "(" ++ show args ++ ")"
 
 sym :: String -> (v -> a) -> Term v a
 sym name fun = App { name = name, fun = fun, args = Nil }
--- Test.hs
+++ Test.hs
@@ -10,61 +10,30 @@ import Test.QuickCheck hiding (numTests, Result)
 import Test.QuickCheck.Gen
 import Utils
 import Family
-
-data BoolAndInt = C Bool Int deriving Eq
-
-instance Ord BoolAndInt where
-  C True x <= C True y = x <= y
-  C False x <= C False y = x <= y
-  C True x <= C False y = x < y
-  C False x <= C True y = x <= y
-
-instance HasTrie BoolAndInt where
-  data BoolAndInt :->: a = B ((Bool, Int) :->: a)
-  trie f = B (trie (\(x, y) -> f (C x y)))
-  untrie (B t) = \(C x y) -> f' (x, y)
-    where f' = untrie t
-  enumerate (B t) = [ (C x y, z) | ((x, y), z) <- enumerate t ]
-
-data IntExpr = Var Var (WithBound BoolAndInt IntExpr) | Const Int | Plus IntExpr IntExpr | Times IntExpr IntExpr | Neg IntExpr deriving (Eq, Ord)
-data Var = X | Y | Z deriving (Eq, Ord, Show)
-instance Show IntExpr where
-  show (Var x _) = show x
-  show (Const n) = show n
-  show (Plus t u) = "(" ++ show t ++ ")+(" ++ show u ++ ")"
-  show (Times t u) = "(" ++ show t ++ ")*(" ++ show u ++ ")"
-  show (Neg t) = "-" ++ show t
+import Term
 
 repeatM = sequence . repeat
 
-tests :: StdGen -> [(Int, Int, Int)]
+type Val = (Int, Int, Int)
+
+tests :: StdGen -> [Val]
 tests g = unGen (repeatM arbitrary) g 5
 
-trees :: Int -> StdGen -> BoolAndInt -> TestResults IntExpr
--- Hmm, we can even easily make the sound depth optimisation this
--- way---just make the code for n+1 tests base itself on the code for
--- n tests. All we really need is union for that. Plus an operation to
--- find out what new representatives appear at depth n of the testing
--- tree.
---
--- Actually---we currently kill a branch when it hasn't been refined
--- for 100 steps, in which case the depth optimisation is perfectly
--- sound! The problematic terms don't get tested any more at higher
--- depth.
-trees numTests gen = f
+trees :: [Val] -> Int -> Int -> TestResults (Term Val Int)
+trees tcs numTests = f
   where f = memo (cutOff numTests . f')
-        base = memo $ \n -> test allTests $ [ Const 0, Const 1 ] ++
-                                            [ Var x (WithBound n (reps (f n))) | x <- [X, Y, Z] ]
-        f' (C False 0) = base (C False 0)
-        f' (C False (n+1)) = base (C False (n+1)) `union` test allTests (liftM3 id [Plus, Times] ts ts ++ map Neg ts)
-          where ts = reps (f (C False n))
-        f' (C True 0) = base (C True 0) `union` test allTests (map Neg (reps (cutOff numTests (base (C True 0)))))
-        f' (C True (n+1)) = base (C True (n+1)) `union` test allTests (map Neg (reps (cutOff numTests (base (C True (n+1))))))
-                                          `union` test allTests (liftM3 id [Plus, Times] ts ts)
-                                          `union` test allTests (map Neg (reps (f (C False (n+1)))))
-          where ts = reps (f (C True n))
-        allTests = tests gen
+        base :: Int -> TestTree (Term Val Int)
+        base = memo $ \n -> test tcs $ [ sym "0" $ const 0, sym "1" $ const 1,
+                                         sym "x" (\(x, _, _) -> x),
+                                         sym "y" (\(_, y, _) -> y),
+                                         sym "z" (\(_, _, z) -> z) ]
+        f' :: Int -> TestTree (Term Val Int)
+        f' 0 = base 0
+        f' (n+1) = base (n+1) `union` test tcs (liftM3 app3 [sym "+" $ const (+), sym "*" $ const (*)] ts ts ++ map (app (sym "-" $ const negate)) ts)
+          where ts = reps (f n)
+                app3 f x y = app (app f x) y
 
+{-
 constraints :: IntExpr -> [(Var, WithBound BoolAndInt IntExpr)]
 constraints (Var v x) = [(v, x)]
 constraints (Const _) = []
@@ -77,22 +46,10 @@ constraints2 t u = merge f fst (constraints t) (constraints u)
 
 instances :: IntExpr -> [IntExpr]
 instances = undefined
-
-instance Eval IntExpr where
-  type TestCase IntExpr = (Int, Int, Int)
-  type Result IntExpr = Int
-  eval (x, _, _) (Var X _) = x
-  eval (_, y, _) (Var Y _) = y
-  eval (_, _, z) (Var Z _) = z
-  eval _ (Const x) = x
-  eval e (Plus x y) = eval e x + eval e y
-  eval e (Times x y) = eval e x * eval e y
-  eval e (Neg x) = negate (eval e x)
+-}
 
 main = do
   seed <- newStdGen
-  let f = trees 100 seed
-      rs = f (C True 2)
-      rs' = f (C False 2)
-  printf "w/ depth optimisation, w/o free unary functions: %d terms, %d classes\n" (length (concat (classes rs'))) (length (classes rs'))
+  let f = trees (tests seed) 100
+      rs = f 2
   printf "w/ depth optimisation: %d terms, %d classes\n" (length (concat (classes rs))) (length (classes rs))
