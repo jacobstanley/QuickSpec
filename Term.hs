9256681fd60ef5a659a58224bf13f4f4e0cda03f
Nick Smallbone <nicsma@chalmers.se>
2010-08-24 20:19:55 +0800

    A more general term structure.
    Ignore-this: 5784217cbcdd1d288b627ea9f3d8e895
    
    darcs-hash:20100824121955-4f525-4ee1d0c30e08687a5e53f7ca48587f496e2e52d3.gz

--- Term.hs
+++ Term.hs
@@ -3,47 +3,37 @@ module Term where
 
 import TestTree
 import Data.Ord
+import Control.Applicative
 
-data Args v a b where
-  Nil :: Args v a a
-  Snoc :: Term v a -> Args v b (a -> c) -> Args v b c
+data Args f a b where
+  Nil :: Args f a a
+  Snoc :: Term f a -> Args f b (a -> c) -> Args f b c
 
-instance Show (Args v a b) where
+instance Show (Args f a b) where
   show Nil = ""
   show (Snoc x Nil) = show x
   show (Snoc x as) = show as ++ "," ++ show x
 
-data Term v a = forall b. App { name :: String, fun :: (v -> b), args :: Args v b a }
+data Term f a = forall b. App { name :: String, fun :: f b, args :: Args f b a }
 
-class Ord (Observation a) => Observe a where
-  type Context a
-  type Observation a
-  observe :: Context a -> a -> Observation a
-
-instance Eq (Term v a) where
+-- FIXME: fix.
+instance Eq (Term f a) where
   t1 == t2 = show t1 == show t2
-instance Ord (Term v a) where
+instance Ord (Term f a) where
   compare = comparing show
 
-instance Show (Term v a) where
+instance Show (Term f a) where
   show App { name = name, args = Nil } = name
   show App { name = name, args = args } = name ++ "(" ++ show args ++ ")"
 
-sym :: String -> (v -> a) -> Term v a
+sym :: String -> f a -> Term f a
 sym name fun = App { name = name, fun = fun, args = Nil }
 
-app :: Term v (a -> b) -> Term v a -> Term v b
+app :: Term f (a -> b) -> Term f a -> Term f b
 app App { name = name, fun = f, args = as } x = App { name = name, fun = f, args = Snoc x as }
 
-evalTerm :: v -> Term v a -> a
-evalTerm v App { fun = f, args = as } = apply v (f v) as
-  where apply :: v -> a -> Args v a b -> b
-        apply v f Nil = f
-        apply v f (Snoc x as) = apply v f as (evalTerm v x)
-
--- FIXME: maybe it would be better to have a different ObservedTerm
--- type and use it in testing instead.
-instance Observe a => Eval (Term v a) where
-  type Result (Term v a) = Observation a
-  type TestCase (Term v a) = (Context a, v)
-  eval (c, v) t = observe c (evalTerm v t)
+evalTerm :: Applicative f => Term f a -> f a
+evalTerm App { fun = f, args = as } = apply f as
+  where apply :: Applicative f => f a -> Args f a b -> f b
+        apply f Nil = f
+        apply f (Snoc x as) = apply f as <*> evalTerm x
